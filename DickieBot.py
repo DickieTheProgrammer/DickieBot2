9a2129dd5dfb7cf720ce9c3aff4cd87dde80e298
Dickie <payne.richard.l@gmail.com>
2021-08-08 14:09:15 -0500

    Barebones connect to discord

--- DickieBot.py
+++ DickieBot.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python3
+#DickieBot: The Impressionable Discord Bot
 
-print("Hello, World!'")
+import os
+import discord
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print(f'{client.user.name} has connected to Discord!')
+
+
+client.run(TOKEN)
