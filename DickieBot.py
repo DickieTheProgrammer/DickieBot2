47ce9fdc1b21061836a07c4cdccbf56105b76157
Dickie <payne.richard.l@gmail.com>
2021-08-23 16:44:04 -0500

    Added ability to add items to inventory

--- DickieBot.py
+++ DickieBot.py
@@ -4,62 +4,87 @@
 import os
 import discord
 import re
-import dbFunctions as db
+from dbFunctions import Connection
 
 from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+DATABASE = os.getenv('DATABASE')
+
+db = Connection(DATABASE)
+
+client = discord.Client()
+botID = None
 
 emoteRegex = [
     # Emotes starting with underscores, retrieve group 3
     re.compile(r"^([_]{1,2}([*]{0,1}|[*]{3})([^_*]*)([*]{0,1}|[*]{3})[_]{1,2})$"),
     # Emotes starting with asterisks, retrieve group 2
     re.compile(r"^([*]{1}|[*]{3})[_]{0,2}([^_*]*)[_]{0,2}([*]{1}|[*]{3})$")
-
-    #re.compile(r"^[_]{1}[^_].+[^-][_]{1}$"),                # _x_, /me converts to this
-    #re.compile(r"[*]{1}[^*].+[^*][*]{1}$"),                 # *x*
-    #re.compile(r"^[_]{2}[*]{1}[^*].+[^*][*]{1}[_]{2}$"),    # __*x*__
-    #re.compile(r"^[*]{1}[_]{2}[^_].+[^_][_]{2}[*]{1}$"),    # *__x__*
-    #re.compile(r"^[*]{3}[_]{2}[^_].+[^_][_]{2}[*]{3}$"),    # ***__x__***
-    #re.compile(r"^[*]{3}[^*].+[^*][*]{3}$"),                # ***x***
-    #re.compile(r"^[_]{2}[*]{3}[^*].+[^*][*]{3}[_]{2}$"),    # __***x***__
-    #re.compile(r"^[*]{1}[_]{1}[^_].+[^_][_]{1}[*]{1}$"),    # *_x_*
-    #re.compile(r"^[_]{1}[*]{1}[^*].+[^*][*]{1}[_]{1}$"),    # _*x*_
 ]
 
 def convertEmote(msg):
     if emoteRegex[0].match(msg):
         # Emote starts with underscore
-        retMsg = '_' + emoteRegex[0].match(msg).group(3) + '_'
+        msgOut = '_' + emoteRegex[0].match(msg).group(3) + '_'
     elif emoteRegex[1].match(msg):
-        retMsg = '_' + emoteRegex[1].match(msg).group(2) + '_'
+        # Emote starts with asterisk
+        msgOut = '_' + emoteRegex[1].match(msg).group(2) + '_'
     else:
-        retMsg = msg
+        # Ain't no emote
+        msgOut = msg
 
-    return(retMsg)
+    return(msgOut)
 
-load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-DATABASE = os.getenv('DATABASE')
+def parseMsg(msg, user, guild, user_id, discriminator):
+    msgOut = None
+    
+    # Normalize emotes to _<words>_
+    msgIn = convertEmote(msg) 
 
-DickieBase = db.Connection(DATABASE)
+    # Collapse message to one line
+    msgIn = re.sub(r'\n',' ',msg) 
 
-client = discord.Client()
+    # Convert notification reference to "$self" variable
+    myself = '<@!' + str(botID) + '>'
+    msgIn = msgIn.replace(myself,'$self') 
+    myself = '<@&' + str(botID) + '>'
+    msgIn = msgIn.replace(myself,'$self') 
+
+    if msgIn.startswith('_gives $self'):
+
+        # Given Inventory item
+        itemRegex = re.compile(r'^[_](?:gives \$self )(.*)[_]$')
+        item = itemRegex.match(msgIn).group(1)
+
+        if not db.addToInventory(guild, user, item, user_id, discriminator):
+            msgOut = 'something went wrong'
+        else:
+            msgOut = '_added %s to his inventory_' % item
+
+    return(msgOut)
 
 @client.event
 async def on_ready():
+    global botID
+    botID = client.user.id
     print(f'{client.user.name} has connected to Discord!')
     print(f'{client.user} is user')
+    print(f'{botID}')
 
 @client.event
 async def on_message(message):
     if message.author == client.user:
         return
+    msgToSend = parseMsg(message.content, message.author.display_name, message.guild.id, message.author.id, message.author.discriminator)
 
+    if msgToSend != None:
+        await message.channel.send(msgToSend)
     #await message.channel.send("""this sends a message""")
-    newMessage = convertEmote(message.content)
 
-    print(f'{message}')
+    #print(f'{message}')
     #print(f'{message.content}')
-    print(f'{newMessage}')
 
 client.run(TOKEN)
 
--- dbFunctions.py
+++ dbFunctions.py
@@ -2,38 +2,61 @@
 #This file handles the interaction with the sqlite database
 
 import sqlite3 as sql
+from datetime import datetime
+from dateutil import tz
 
 class Connection:
 
-    def __init__(self,db):
+    def __init__(self, db):
         self.db = db
         
         self.conn = sql.connect(self.db)
 
         c = self.conn.cursor()
 
-        c = self.conn.execute("""
-            CREATE TABLE IF NOT EXISTS FACTS (
-                ID          INTEGER PRIMARY KEY AUTOINCREMENT,
-                MSG         VARCHAR2(2000),
-                TRIGGER     VARCHAR2(2000),
-                SFW         INTEGER DEFAULT 0,
-                DELETED     INTEGER DEFAULT 0,
-                CNT         INTEGER DEFAULT 0,
-                CREATOR     VARCHAR2(37),
-                CREATED     VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS CST */
-                LASTCALLED  VARCHAR2(23)  /* YYYY-MM-DD HH:MI:SS CST */
-            )
-        """)
+        ###TESTING ONLY
+        try:
+            c = self.conn.execute("""drop table FACTS""")
+        except:
+            None
+        
+        try:
+            c = self.conn.execute("""drop table HISTORY""")
+        except:
+            None
+        
+        try:
+            c = self.conn.execute("""drop table INVENTORY""")
+        except:
+            None
+        ###TESTING ONLY
 
         c = self.conn.execute("""
+            CREATE TABLE IF NOT EXISTS FACTS (
+                ID              INTEGER PRIMARY KEY AUTOINCREMENT,
+                MSG             VARCHAR2(2000) NOT NULL,
+                TRIGGER         VARCHAR2(2000),
+                SFW             INTEGER NOT NULL DEFAULT 0,
+                DELETED         INTEGER NOT NULL DEFAULT 0,
+                CNT             INTEGER NOT NULL DEFAULT 0,
+                CREATOR         VARCHAR2(37),
+                CREATED         VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
+                LASTCALLED      VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
+                CREATOR_ID      VARCHAR2(37),
+                DISCRIMINATOR   INTEGER
+            )
+        """)
+        c = self.conn.execute("""
             CREATE TABLE IF NOT EXISTS HISTORY (
-                ID          INTEGER PRIMARY KEY AUTOINCREMENT,
-                FACT        INTEGER NOT NULL,
-                OLDMSG      VARCHAR2(2000),
-                NEWMSG      VARCHAR2(2000),
-                USER        VARCHAR2(37),
-                EDITDATE    VARCHAR2(23),
+                ID              INTEGER PRIMARY KEY AUTOINCREMENT,
+                FACT            INTEGER NOT NULL,
+                OLDMSG          VARCHAR2(2000),
+                NEWMSG          VARCHAR2(2000),
+                DELETED         INTEGER NOT NULL DEFAULT 0,
+                USER            VARCHAR2(37),
+                EDITDATE        VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
+                USER_ID         VARCHAR2(37),
+                DISCRIMINATOR   INTEGER,
                 FOREIGN KEY (FACT)
                     REFERENCES FACTS (id)
             )
@@ -45,8 +68,31 @@ class Connection:
                 GUILD       INTEGER NOT NULL,
                 ITEM        VARCHAR2(100),
                 USER        VARCHAR2(37),
-                DATEADDED   VARCHAR2(23)
+                DATEADDED   VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
+                USER_ID      VARCHAR2(37),
+                DISCRIMINATOR   INTEGER
             )
         """)
 
+    def addToInventory(self, guild, user, item, userID, disc):
+        success = False
+        currentDateStr = datetime.now(tz.gettz('America/Chicago')).strftime('%Y-%m-%d %H:%M:%S %Z')
+        
+        try:
+            c = self.conn.execute("""
+                INSERT INTO INVENTORY (GUILD,ITEM,USER,DATEADDED,USER_ID,DISCRIMINATOR)
+                values(?,?,?,?,?,?)
+                """,(guild,item,user,currentDateStr,userID,disc))
+                
+            self.conn.commit()
 
+            success = True
+        except Exception as e:
+            success = False
+            print(e)
+        
+        return(success)
+
+    def getInventory(self,guild):
+        allItems = None
+        success = False
