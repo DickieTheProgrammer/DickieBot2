78ba67e56764f5c8fcfa4f9c5a22985d4034359c
Dickie <payne.richard.l@gmail.com>
2021-08-28 09:05:18 -0500

    Refactored msg parsing. Implemented fact variables, fact adding, and inventory querying

--- DickieBot.py
+++ DickieBot.py
@@ -5,8 +5,10 @@ import os
 import discord
 import re
 import random
+import string
 
 from discord import member, utils
+from discord.channel import TextChannel
 
 from dbFunctions import Connection
 from dotenv import load_dotenv
@@ -18,12 +20,14 @@ DATABASE = os.getenv('DATABASE')
 intents = discord.Intents.all()
 
 db = Connection(DATABASE)
-bot = commands.Bot(command_prefix='!')
+help_command = commands.DefaultHelpCommand(no_category='Commands')
+bot = commands.Bot(command_prefix='!', description='DickieBot: The impressionable Discord bot!', help_command=help_command)
 
 botID = None
 botName = None
 
 roleIDs = {}
+lastFact = {}
 
 nothingList = [
     "https://giphy.com/embed/l41YlPKMCfNoseS8E",
@@ -36,7 +40,7 @@ nothingList = [
     "https://giphy.com/embed/QZOaeparxsNOfKWbER",
     "https://giphy.com/embed/gjIvS6VpFQKNP0ZcvJ",
     "https://c.tenor.com/q-E5wj1K6OYAAAAM/monsters-inc-sully.gif"
- ]
+]
 
 emoteRegex = [
     # Emotes starting with underscores, retrieve group 2
@@ -52,41 +56,21 @@ def convertEmote(msg):
     elif emoteRegex[1].match(msg):
         # Emote starts with asterisk
         msgOut = '_' + emoteRegex[1].match(msg).group(2) + '_'
+    elif msg.startswith('/me '):
+        msgOut = '_' + msg[4:] + '_'
     else:
         # Ain't no emote
         msgOut = msg
 
     return(msgOut)
 
-def parseMsg(msg, user, guild, user_id, discriminator):
-    msgOut = None
-    
-    # Standardize emotes to _<words>_
-    msgIn = convertEmote(msg) 
-
-    # Collapse message to one line
-    msgIn = re.sub(r'\n',' ',msg) 
-
-    # Convert notification reference to "$self" variable
+def mentionToSelfVar(msgIn,botRole):
     # Nick ping
-    nick = '<@!' + str(botID) + '>'
-    msgIn = msgIn.replace(nick,'$self') 
+    nick = '<@' + str(botID) + '>'
+    msgOut = msgIn.replace('!','').replace(nick,'$self') 
     # Role ping
-    role = '<@&' + str(roleIDs[guild]) + '>'
-    msgIn = msgIn.replace(role,'$self') 
-    print (msgIn)
-    print(roleIDs[guild])
-
-    if msgIn.startswith('_gives $self'):
-
-        # Given Inventory item
-        itemRegex = re.compile(r'^_{1}(?:gives \$self )(.*)_{1}$')
-        item = itemRegex.match(msgIn).group(1)
-
-        if not db.addToInventory(guild, user, item, user_id, discriminator):
-            msgOut = 'something went wrong'
-        else:
-            msgOut = '_added %s to his inventory_' % item
+    role = '<@&' + str(botRole) + '>'
+    msgOut = msgIn.replace(role,'$self') 
 
     return(msgOut)
 
@@ -97,12 +81,13 @@ async def on_ready():
     global botName
     botName = bot.user.name
     global roleIDs
+
     for gld in bot.guilds: 
         roleIDs[gld.id]=utils.get(gld.roles,name=botName).id
 
     print(f'{bot.user.name} has connected to Discord!')
     print(f'{bot.user} is user')
-    print(f'{botID}')
+    print(f'{botID} is ID')
 
 @bot.event
 async def on_guild_join(guild):
@@ -112,55 +97,77 @@ async def on_guild_join(guild):
 
 @bot.event
 async def on_message(message):
+    msgToSend = None
+
     if message.author == bot.user:
         return
 
-    print(f'{message.content}')
+    print(f'{message}')
 
-    msgToSend = parseMsg(message.content, message.author.display_name, message.guild.id, message.author.id, message.author.discriminator)
+    # Standardize emotes to _<words>_
+    msgIn = convertEmote(message.content) 
+
+    # Collapse message to one line
+    msgIn = re.sub(r'\n',' ',msgIn) 
+
+    # Convert notification reference to "$self" variable
+    msgIn = mentionToSelfVar(msgIn, roleIDs[message.guild.id])
+
+    if msgIn.startswith('_gives $self') and msgIn.endswith('_'):
+        # Given Inventory item
+        itemRegex = re.compile(r'^_{1}(?:gives \$self )(.*)_{1}$')
+        item = itemRegex.match(msgIn).group(1)
+
+        if not db.addToInventory(message.guild.id, message.author.display_name, item, message.author.id):
+            msgToSend = 'something went wrong'
+        else:
+            msgToSend = '_added %s to his inventory_' % item
+    elif not msgIn.startswith('!'):
+        nsfwTag = 1 if message.channel.is_nsfw() else 0
+
+        # Check to see if factoid triggered
+        msgInParts = msgIn.split('$self')
+        msgIn = '$self'.join(e.strip(string.punctuation).lower() for e in msgInParts).strip()
+
+        id, msgToSend = db.getFact(msgIn,nsfwTag)
+        lastFact[message.guild.id] = id
+
+        if id != None:
+            db.updateLastCalled(id)
+
+        # Replace $nick variables with message author
+        try:
+            msgToSend = msgToSend.replace('$nick','<@!' + str(message.author.id) + '>') 
+        except:
+            None
+
+        # Replace $rand variables each with random guild member
+        guildMembers = message.guild.members
+        randCount = msgToSend.count('$rand') if msgToSend != None else 0
+        for i in range(randCount):
+            randUser = '<@!' + str(random.sample(guildMembers,1)[0].id) + '>'
+            msgToSend = msgToSend.replace('$rand', randUser, 1)
+
+        # Replace $item variables each with random inventory item
+        itemCount = msgToSend.count('$item') if msgToSend != None else 0
+        for i in range(itemCount):
+            randItem = db.getInventoryItem(message.guild.id)
+            msgToSend = msgToSend.replace('$item', randItem, 1)
+                
 
     if msgToSend != None:
         await message.channel.send(msgToSend)
 
     await bot.process_commands(message)
 
-@bot.command(name = 'source')
-async def source(ctx):
-    await ctx.send("https://github.com/DickieTheProgrammer/DickieBot2")
-
-@bot.command(name = 'inv', aliases=['inventory','stuff'])
-# add @commands.cooldown(2, 20, commands.BucketType.user) here?
-async def inv(ctx):
-    print(ctx.guild.id)
-    itemList = db.getInventory(ctx.guild.id)
-
-    if itemList == None:
-        await ctx.send("Something went wrong fetching inventory.")
-        return
-
-    deTupledList = [' '.join(elem) for elem in itemList]
-
-    listLen = len(deTupledList)
-    if listLen == 0:
-        msgToSend = random.sample(nothingList,1)[0]
-    else:
-        msgToSend = "I have:\n\t○ " + "\n\t○ ".join(deTupledList[0:5])
-
-        if listLen > 5:
-            msgToSend = msgToSend + "\nand, like, " + str(listLen-5) + " other thing"
-        if listLen > 6:
-            msgToSend = msgToSend + "s"
-
-    print(msgToSend)
-    await ctx.send(msgToSend)
-
-@bot.command(name = 'roll')
+######### GENERAL COMMANDS #########
+@bot.command(name = 'roll', description = f'Simulates dice rolls. Syntax is !roll {{number}}d{{number}}.', brief = f'Rolls dem bones - {{number}}d{{number}}')
 async def roll(ctx, dice: str):
     # Rolls a dice in NdN format. 
     try:
         rolls, limit = map(int, dice.split('d'))
     except Exception:
-        await ctx.send('Format has to be in \{number\}**d**\{number\}')
+        await ctx.send(f'Format has to be in {{number}}**d**{{number}}')
         return
 
     if rolls>25:
@@ -172,5 +179,96 @@ async def roll(ctx, dice: str):
 
     await ctx.send(msgToSend)
 
-bot.run(TOKEN)
+@bot.command(name = 'src', aliases=['source','sauce'], description='Returns GitHub source URL.', brief='Returns source URL')
+async def source(ctx):
+    await ctx.send("<https://github.com/DickieTheProgrammer/DickieBot2>")
+    
+######### INVENTORY COMMANDS #########
+@bot.command(name = 'inv', aliases=['inventory','stuff','items'], description="Returns current guild's inventory. Aliases: inventory, stuff.", brief='Displays inventory')
+# add @commands.cooldown(2, 20, commands.BucketType.user) here?
+async def inv(ctx):
+    itemList = db.getInventory(ctx.guild.id)
 
+    if itemList == None:
+        await ctx.send("Something went wrong fetching inventory.")
+        return
+
+    deTupledList = [' '.join(elem) for elem in itemList]
+    listLen = len(deTupledList)
+
+    # Randomly order list to give "first in" items some visibility
+    deTupledList = random.sample(deTupledList,listLen)
+
+    if listLen == 0:
+        msgToSend = random.sample(nothingList,1)[0]
+    else:
+        msgToSend = "I have:\n\t○ " + "\n\t○ ".join(deTupledList[0:5])
+
+        if listLen > 5:
+            msgToSend = msgToSend + "\nand, like, " + str(listLen-5) + " other thing"
+        if listLen > 6:
+            msgToSend = msgToSend + "s"
+
+    await ctx.send(msgToSend)
+
+@bot.command(name = 'whogave', aliases = ['whogaveyou','whogaveu'], description = 'Tells who gave me an item', brief = 'Who gave me a thing?')
+async def whogave(ctx, *, item):
+    donors = db.getItemDonor(ctx.guild.id,item)
+    resolvedDonors = []
+
+    if donors == None:
+        msgToSend = "Something went wrong finding the donor of this item."
+    elif len(donors) == 0:
+        msgToSend = "I don't have that."
+    else:
+        for donor in donors: resolvedDonors.append('<@!' + donor + '>')
+        msgToSend = "It was %s" % ' and '.join(resolvedDonors)
+    
+    await ctx.send(msgToSend)
+        
+######### FACTOID COMMANDS #########
+@bot.command(name = 'on', 
+            aliases = ['onnsfw'], 
+            description = f"""Assign a response to triggering phrase. !on<nsfw> {{trigger}} -say {{response}}. 
+            Using !onnsfw marks the response as NSFW and will not be triggered in SFW channels.
+            Use $self to refer to the bot in the trigger. i.e. !on "Hi $self" -say Hello. 
+            Use $rand, $nick, and $item in response to sub in a random user, the triggering user, and an inventory item, respectively.
+            $item consumes the inventory item.""", 
+            brief = 'Teach me to respond to something')
+async def on(ctx, *, args):
+    parts = args.split('-say')
+    if len(parts) <= 1:
+        msgToSend = 'Usage is !on<nsfw> trigger -say response'
+
+    trigger = convertEmote(parts[0]).strip()
+    botCommand = True if trigger.startswith('!') else False
+
+    cleanTrigger = mentionToSelfVar(trigger, roleIDs[ctx.guild.id])
+    triggerParts = cleanTrigger.split('$self')
+    cleanTrigger = '$self'.join(e.strip(string.punctuation).lower() for e in triggerParts).strip()
+
+    if botCommand:
+        msgToSend = "I'm not remembering bot commands."
+    elif len(cleanTrigger) <= 3:
+        msgToSend = 'Trigger must be >= 4 alphanumeric characters'
+    else:    
+        if trigger.startswith('_') and trigger.endswith('_'):
+            cleanTrigger = '_' + trigger + '_'
+
+        response = ''.join(parts[1:]).strip()
+
+        nsfw = 1 if ctx.invoked_with == 'onnsfw' else 0
+
+        success, known = db.addFact(cleanTrigger, response, nsfw, ctx.message.author.display_name, ctx.message.author.id)
+
+        if success:
+            msgToSend = f"""Ok. When I see "{trigger}" I'll say "{response}\""""
+        else:
+            if known:
+                msgToSend = 'Oh, I already know that.'
+            else:
+                msgToSend = 'Something went wrong adding this factoid.'
+
+    await ctx.send(msgToSend)
+
+bot.run(TOKEN)
--- dbFunctions.py
+++ dbFunctions.py
@@ -2,6 +2,7 @@
 #This file handles the interaction with the sqlite database
 
 import random
+import inspect
 import sqlite3 as sql
 from datetime import datetime
 from dateutil import tz
@@ -16,20 +17,20 @@ class Connection:
         c = self.conn.cursor()
 
         ###TESTING ONLY
-        try:
-            c = self.conn.execute("""drop table FACTS""")
-        except:
-            None
+        #try:
+        #    c = self.conn.execute("""drop table FACTS""")
+        #except:
+        #    None
         
         try:
             c = self.conn.execute("""drop table HISTORY""")
         except:
             None
         
-        try:
-            c = self.conn.execute("""drop table INVENTORY""")
-        except:
-            None
+        #try:
+        #    c = self.conn.execute("""drop table INVENTORY""")
+        #except:
+        #    None
         ###TESTING ONLY
 
         c = self.conn.execute("""
@@ -37,14 +38,14 @@ class Connection:
                 ID              INTEGER PRIMARY KEY AUTOINCREMENT,
                 MSG             VARCHAR2(2000) NOT NULL,
                 TRIGGER         VARCHAR2(2000),
-                SFW             INTEGER NOT NULL DEFAULT 0,
+                NSFW            INTEGER NOT NULL DEFAULT 0,
                 DELETED         INTEGER NOT NULL DEFAULT 0,
                 CNT             INTEGER NOT NULL DEFAULT 0,
                 CREATOR         VARCHAR2(37),
                 CREATED         VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
                 LASTCALLED      VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
                 CREATOR_ID      VARCHAR2(37),
-                DISCRIMINATOR   INTEGER
+                UNIQUE (MSG, TRIGGER)
             )
         """)
         c = self.conn.execute("""
@@ -57,7 +58,6 @@ class Connection:
                 USER            VARCHAR2(37),
                 EDITDATE        VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
                 USER_ID         VARCHAR2(37),
-                DISCRIMINATOR   INTEGER,
                 FOREIGN KEY (FACT)
                     REFERENCES FACTS (id)
             )
@@ -70,33 +70,36 @@ class Connection:
                 ITEM        VARCHAR2(100),
                 USER        VARCHAR2(37),
                 DATEADDED   VARCHAR2(23), /* YYYY-MM-DD HH:MI:SS TZ */
-                USER_ID      VARCHAR2(37),
-                DISCRIMINATOR   INTEGER
+                USER_ID      VARCHAR2(37)
             )
         """)
 
-    def addToInventory(self, guild, user, item, userID, disc):
+    def getCurrentDateTime(self):
+        return(datetime.now(tz.gettz('America/Chicago')).strftime('%Y-%m-%d %H:%M:%S %Z'))
+
+    def addToInventory(self, guild, user, item, userID):
         success = False
-        currentDateStr = datetime.now(tz.gettz('America/Chicago')).strftime('%Y-%m-%d %H:%M:%S %Z')
         
         try:
             c = self.conn.execute("""
-                INSERT INTO INVENTORY (GUILD,ITEM,USER,DATEADDED,USER_ID,DISCRIMINATOR)
+                INSERT INTO INVENTORY (GUILD,ITEM,USER,DATEADDED,USER_ID)
                 values(?,?,?,?,?,?)
-                """,(guild,item,user,currentDateStr,userID,disc))
+                """,(guild,item,user,self.getCurrentDateTime(),userID))
                 
             self.conn.commit()
 
+            print('Given [' + item + ']')
             success = True
         except Exception as e:
-            success = False
+            print(inspect.stack()[0][3])
+            print(inspect.stack()[1][3])
             print(e)
         
         return(success)
 
     def getInventory(self,guild):   
         itemList = None  
-        print(guild)   
+        
         try:
             c = self.conn.execute("""
                 select ITEM from INVENTORY where GUILD = ?
@@ -104,6 +107,129 @@ class Connection:
             
             itemList = c.fetchall()
         except Exception as e:
+            print(inspect.stack()[0][3])
+            print(inspect.stack()[1][3])
             print(e)
 
         return(itemList)
+
+    def getInventoryItem(self,guild):
+        itemList = None
+        item = None
+
+        try:
+            c = self.conn.execute("""
+                select ITEM, ID from INVENTORY where GUILD = ?
+                order by RANDOM() limit 1
+                """, (guild,))
+            
+            itemList = c.fetchall()
+
+            item = itemList[0][0] if len(itemList)==1 else 'nothing'
+
+            if item != 'nothing':
+                id = itemList[0][1]
+
+                c = self.conn.execute("""
+                    delete from INVENTORY where ID = ?
+                """,(id,))
+
+                self.conn.commit()
+
+        except Exception as e:
+            print(inspect.stack()[0][3])
+            print(inspect.stack()[1][3])
+            print(e)
+
+        return(item)
+
+    def getItemDonor(self,guild,item):
+        donors = []
+
+        try:
+            c = self.conn.execute("""select USER_ID from INVENTORY where ITEM = ? and GUILD = ?""",(item,guild))
+
+            results = c.fetchall()
+            results = [' '.join(elem) for elem in results]
+            if len(results)>0:
+                for i in results: donors.append(i)
+                donors = list(set(donors))
+        except Exception as e:
+            print(inspect.stack()[0][3])
+            print(inspect.stack()[1][3])
+            print(e)
+            donors = None
+
+        return(donors)
+
+    def getRandomFact(self,nsfw):
+        id = None
+        msgOut = None
+
+        sqlIn= [0] if nsfw == 0 else [0,1]
+
+        try:
+            c = self.conn.execute(f"""
+                select ID, MSG from facts where DELETED = 0 and TRIGGER is null and NSFW in ("""+','.join(sqlIn)+""") order by random() limit 1
+            """,(sqlIn))
+
+            results = c.fetchall()
+            id = results[0][0]
+            msgOut = results[0][1]
+        except Exception as e:
+            print(inspect.stack()[0][3])
+            print(inspect.stack()[1][3])
+            print(e)
+
+        return(id,msgOut)
+
+    def addFact(self,trigger,response,nsfw,creator,creatorID):
+        success = False
+        known = False
+
+        try:
+            c = self.conn.execute("""
+                insert into FACTS (MSG, TRIGGER, NSFW, CREATOR, CREATED, CREATOR_ID)
+                values (?,?,?,?,?,?)
+            """, (response, trigger, nsfw, creator, self.getCurrentDateTime(), creatorID))
+            self.conn.commit()
+
+            print('Remembering [' + trigger + '] is [' + response + ']')
+            success = True
+        except sql.IntegrityError:
+            success = False
+            known = True
+        except Exception as e:
+            print(inspect.stack() + ': ' + e)
+
+        return(success, known)
+
+    def getFact(self,trigger,nsfw):
+        success = False
+        msgOut = None
+        id = None
+
+        sqlIn= [0] if nsfw == 0 else [0,1]
+
+        try:
+            c = self.conn.execute(f"""
+                select ID, MSG from FACTS where DELETED = 0 and TRIGGER IS NOT NULL and TRIGGER = ? and NSFW in ("""+','.join(str(n) for n in sqlIn)+""") order by random() limit 1
+            """, (trigger,))
+            results = c.fetchall()
+            if len(results)==0:
+                id, msgOut = None, None
+            else:
+                id, msgOut = results[0][0], results[0][1]
+        except Exception as e:
+            print(inspect.stack()[0][3])
+            print(inspect.stack()[1][3])
+            print(e)
+
+        return(id,msgOut)
+
+    def updateLastCalled(self,id):
+        try:
+            c = self.conn.execute("""update FACTS set LASTCALLED=?, CNT = CNT + 1 where ID = ?""",(self.getCurrentDateTime(),id))
+            self.conn.commit()
+        except:
+            None
